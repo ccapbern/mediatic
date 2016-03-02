f85ca05ac9d2a141b3cb78cdba8cde9c70d3c924
Cyril CAPBERN <ccapbern@asulia.fr>
2016-03-02 17:02:15 +0100

    Doesn't work need some modification on common.js

--- maquette/js/common.js
+++ maquette/js/common.js
@@ -2,16 +2,34 @@
 
 $('document').ready(function() {
 
-	$('#edit').change(function() {
-		var edit = $(this);
-		if (edit.is(':checked')) {
-			$('.inputDisabled').each(function () {
-				$(this).prop("disabled", false);
-			})
-		} else {
-			$('.inputDisabled').each(function () {
-				$(this).prop("disabled", true);
-			})
-		}
+	var d = new Date();
+	var currentDay = (d.getDate() < 10) ? '0' + d.getDate() : d.getDate();
+	var currentMonth = (d.getMonth() + 1 < 10) ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1);
+	var currentYear = d.getFullYear();
+	$('.startDate').val(currentYear + '-' + currentMonth + '-' + currentDay);
+
+	// Edit mode
+	$('.edit').click(function() {
+		$('.editOff').css('display', 'none');
+		$('.editOn').css('display', 'block')
+		$('.inputDisabled').each(function() {
+			$(this).prop("disabled", false);
+		})
+	});
+
+	$('.cancel').click(function() {
+		$('.editOff').css('display', 'block');
+		$('.editOn').css('display', 'none')
+		$('.inputDisabled').each(function() {
+			$(this).css('display', 'none');
+		})
+	});
+
+	$('.save').click(function() {
+		$('.editOff').css('display', 'block');
+		$('.editOn').css('display', 'none')
+		$('.inputDisabled').each(function() {
+			$(this).prop("disabled", true);
+		})
 	});
 });

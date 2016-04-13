98cb89a0f896ffbf678e55abb99679620d74630a
jeremymaniere <j.maniere17@gmail.com>
2016-04-13 10:40:44 +0200

    http configuration for post

--- maquette/app/app.js
+++ maquette/app/app.js
@@ -2,12 +2,16 @@
 // CrÃ©ation du module app avec les dÃ©pendances :
 //  - ng-route : pour gÃ©rer des URL diffÃ©rentes
 
-angular.module('App', ['ngRoute', 'ModuleMedia','ModuleMenu','ModuleAdherent','ModuleSidebar']);
+angular.module('App', ['ngRoute', 'ModuleMedia', 'ModuleMenu', 'ModuleAdherent', 'ModuleSidebar']);
 
 // Configuration du module app
 // => Injection du Provider du service $route afin de le configurer.
-angular.module('App').config(function ($routeProvider) {
+angular.module('App').config(['$routeProvider', function ($routeProvider) {
     $routeProvider.otherwise({
         redirectTo: '/media'
     });
-});
+}]);
+
+angular.module('App').run(['$http', function ($http) {
+    $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+}]);

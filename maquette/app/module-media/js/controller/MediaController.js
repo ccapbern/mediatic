4c5df77735eed2ec54f3bff78337b85e2509b21e
Thomas Nohaic <thomas.nohaic@gmail.com>
2016-04-14 12:44:18 +0200

    Fix de la pagination des tableaux des emprunts des médias et des adhérents

--- maquette/app/module-media/js/controller/MediaController.js
+++ maquette/app/module-media/js/controller/MediaController.js
@@ -11,16 +11,48 @@ angular.module('ModuleMedia').controller('MediaController', ['$rootScope', '$rou
 
         myCtrl.media = undefined;
 
+        myCtrl.pageEmprunt = 0;
+        myCtrl.emprunteursTotaux = [];
+        myCtrl.emprunteurs;
+        var nbPageEmprunt = 0;
+        var nbEmpruntsParPage = 10;
+        
         // Récuparation de l'identifiant qui est le paramètre 'idLivre' de la route
         var id = $routeParams.media_id;
 
+        
         MediaService.getMedia(id).then(function (media) {
             myCtrl.media = media;
+            if (myCtrl.media.emprunteurs != undefined)
+            	myCtrl.emprunteursTotaux = myCtrl.media.emprunteurs;
+            
+           // Calcul du nombre de pages d'emprunts pour la pagination du tableau des emprunts d'un média 
+           nbPageEmprunt = Math.ceil(myCtrl.emprunteursTotaux.length / nbEmpruntsParPage);
+           myCtrl.pagesEmpruntPossibles = [];
+           for (var i = 0; i < nbPageEmprunt; i ++){
+        	   myCtrl.pagesEmpruntPossibles.push(i);
+           }
+           myCtrl.emprunteurs = [];
+           for (var i = myCtrl.pageEmprunt * nbEmpruntsParPage; i < myCtrl.pageEmprunt + nbEmpruntsParPage && i < myCtrl.emprunteursTotaux.length; i++){
+        	   myCtrl.emprunteurs.push(myCtrl.emprunteursTotaux[i]);
+           }
+            
         }, function () {
             // En cas d'erreur
             myCtrl.media = -1;
         });
         
+        
+        myCtrl.changePageEmprunt = function (pageEmprunt) {
+            if (pageEmprunt >= 0 && pageEmprunt < nbPageEmprunt) {
+                myCtrl.pageEmprunt = pageEmprunt;
+                
+                myCtrl.emprunteurs = [];
+                for (var i = myCtrl.pageEmprunt * nbEmpruntsParPage; i < myCtrl.pageEmprunt + nbEmpruntsParPage && i < myCtrl.emprunteursTotaux.length; i++){
+                	myCtrl.emprunteurs.push(myCtrl.emprunteursTotaux[i]);
+                }
+            }
+        };
         
         myCtrl.adherentsActifs = undefined;
 
